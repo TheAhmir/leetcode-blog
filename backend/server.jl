7e2bd0b90e47850d403d9d08da70b4d61de64601
Ahmir Postell <postell.ahmir@gmail.com>
2025-03-28 13:44:06 -0400

    adjusted backend

--- backend/server.jl
+++ backend/server.jl
@@ -3,7 +3,7 @@ using Genie
 include("src/backend.jl")
 
 Genie.config.server_host = "0.0.0.0"
-Genie.config.server_port = 8000
+Genie.config.server_port = get(ENV, "PORT", "8000") |> parse(Int)
 Genie.config.run_as_server = true
 
 up()
