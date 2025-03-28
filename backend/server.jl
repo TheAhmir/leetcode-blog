f595a898842e4b7bbbbf15428cd3bf4f53e37624
Ahmir Postell <postell.ahmir@gmail.com>
2025-03-28 14:17:23 -0400

    setup env file port

--- backend/server.jl
+++ backend/server.jl
@@ -1,9 +1,16 @@
 using Genie
+using DotEnv
+
+DotEnv.load!()
 
 include("src/backend.jl")
 
 Genie.config.server_host = "0.0.0.0"
-Genie.config.server_port = get(ENV, "PORT", "8000") |> parse(Int)
+if "PORT" in keys(ENV)
+    Genie.config.server_port = parse(Int, ENV["PORT"]) 
+else
+    Genie.config.server_port = 8000
+end
 Genie.config.run_as_server = true
 
 up()
